43450c4fc424faf56c842be7a7408b84f2ce7ae2
SpezialK-dev <spezcialK@protonmail.com>
2021-10-08 22:40:53 +0200

    initial commit

---
+++ src/com/IpToGameCode/GameCodeToIp.java
@@ -0,0 +1,7 @@
+package com.IpToGameCode;
+
+public class GameCodeToIp {
+    public GameCodeToIp(){
+        //this will turn the ip into a gamecode
+    }
+}
---
+++ src/com/IpToGameCode/toGameCode.java
@@ -0,0 +1,7 @@
+package com.IpToGameCode;
+
+public class toGameCode {
+    public toGameCode(){
+        //this is supposed to take an ip address and then convert it into a game code
+    }
+}
