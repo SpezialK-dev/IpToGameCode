13b08e60b3395cd6c061a908924fa949b98945a9
SpezialK-dev <spezcialK@protonmail.com>
2021-10-10 16:53:29 +0200

    added the splitting of the string

--- src/com/IpToGameCode/toGameCode.java
+++ src/com/IpToGameCode/toGameCode.java
@@ -2,12 +2,19 @@ package com.IpToGameCode;
 
 import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.util.regex.Pattern;
 
 public class toGameCode extends Main{
     public static String conversion(InetAddress add){
         System.out.println(add.getHostAddress()); // I also just want to see what this gives us //this shold be it and should work even better
         String theCurrentIpAddressAsString = add.getHostAddress();
-        String IpAddressArray[] = theCurrentIpAddressAsString.split(".");
+        String IpAddressArray[] = theCurrentIpAddressAsString.split(Pattern.quote("."));
+        for (String currnetInput: IpAddressArray) {
+            System.out.println("the current Number : " + currnetInput);
+        }
+
+        
+
         return "";//todo add something to reutnr
     }
     public String ConvertIndividualAddresToWords(String partOfTheAddress){
