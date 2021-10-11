19922afdcd0397437d68996dc17f457d5d31fdf5
SpezialK-dev <spezcialK@protonmail.com>
2021-10-11 13:26:42 +0200

    added the split for each number we now have a way to get each number, and we just need to take that number and convert it into a value

--- src/com/IpToGameCode/toGameCode.java
+++ src/com/IpToGameCode/toGameCode.java
@@ -14,17 +14,30 @@ public class toGameCode extends Main{
 
         //iterates through every part of the ip and then uses it
         for (String currnetInput: IpAddressArray) {
+
             System.out.println("the current Number : " + currnetInput);
+
             ConvertIndividualAddresToWords(currnetInput);
         }
 
         return "";//todo add something to reutnr
     }
+    //converts the individual number into a string
     public static String ConvertIndividualAddresToWords(String partOfTheAddress){
         //takes the number and converts it into a float and devides that float by 10 so that I can seperate the first and second part later
         float ogPartofAdd=Float.parseFloat(partOfTheAddress);
-        float endAdd = ogPartofAdd/10;
+        String endAdd = Float.toString(ogPartofAdd/10);//this splits it I could have also used Big number but I dont need nay of those functions and this should not take that many cpu cycles to convert this back to an int later
+
         System.out.println("as a float : " + endAdd);//can be removed later is only there for visual output
+
+        //splitting it into the respectiv numbers
+        String splitVal[] = endAdd.split(Pattern.quote("."));//the first part is the hundered digit and the second part is only the first number
+        //I can hardcode the valous of the array because I know that they will always be two
+
+        //todo remove the priniting
+        System.out.println( "part 1: "+splitVal[0]);
+        System.out.println( "part 2: "+splitVal[1]);
+
 
 
         return "";//todo replace this with an actuall return and not just void
