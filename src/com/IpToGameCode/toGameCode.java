44cefd73d6f84804ede8aa81e8a748a9dab3f709
SpezialK-dev <spezcialK@protonmail.com>
2021-10-21 22:11:53 +0200

    added Documentation to the GamecodetoIp class and removed todos also fixed typos

--- src/com/IpToGameCode/toGameCode.java
+++ src/com/IpToGameCode/toGameCode.java
@@ -4,53 +4,44 @@ import java.net.InetAddress;
 import java.util.regex.Pattern;
 
 public class toGameCode extends Main{
-    //todo create a array with all of the letters the first letter is n and after that normal
     final static String[] allLetersinOrder = {"N","A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"};//the list of all letters in the order that they will be assigned
     public static String conversion(InetAddress add){
-        //ussless print command
+        //useless print command
         String endGameCode ="";
-        System.out.println(add.getHostAddress()); // I also just want to see what this gives us //this shold be it and should work even better
 
-        //converts the Address objket into a string so that I can use it later also splits it into its own parts
+        //converts the Address objects into a string so that I can use it later also splits it into its own parts
         String theCurrentIpAddressAsString = add.getHostAddress();
         String IpAddressArray[] = theCurrentIpAddressAsString.split(Pattern.quote("."));
 
         //iterates through every part of the ip and then uses it
-        for (String currnetInput: IpAddressArray) {
+        for (String currentInput: IpAddressArray) {
 
-            System.out.println("the current Number : " + currnetInput);
-
-            endGameCode = endGameCode +ConvertIndividualAddresToWords(currnetInput);
+            //takes all the individual numbers and runs them to the second methode
+            endGameCode = endGameCode + convertIndividualAddresToWords(currentInput);
         }
+        //just there as feedback can be removed
         System.out.println("the final String is : " + endGameCode);//just prints the final game code as a test
 
 
-        return "";//todo add something to reutnr
+        return endGameCode;
     }
     //converts the individual number into a string
-    public static String ConvertIndividualAddresToWords(String partOfTheAddress){
+    public static String convertIndividualAddresToWords(String partOfTheAddress){
         String finalString = "";
-        //takes the number and converts it into a float and devides that float by 10 so that I can seperate the first and second part later
+        //takes the number and converts it into a float and divides that float by 10 so that I can separate the first and second part later
         float ogPartofAdd=Float.parseFloat(partOfTheAddress);
-        String endAdd = Float.toString(ogPartofAdd/10);//this splits it I could have also used Big number but I dont need nay of those functions and this should not take that many cpu cycles to convert this back to an int later
+        String endAdd = Float.toString(ogPartofAdd/10);//this splits it I could have also used Big number, but I don't need nay of those functions and this should not take that many cpu cycles to convert this back to an int later
 
-        System.out.println("as a float : " + endAdd);//can be removed later is only there for visual output
+        //splitting it into the respective numbers
+        String splitVal[] = endAdd.split(Pattern.quote("."));//the first part is the hundred digit and the second part is only the first number
+        //I can hardcode the values of the array because I know that they will always be two
 
-        //splitting it into the respectiv numbers
-        String splitVal[] = endAdd.split(Pattern.quote("."));//the first part is the hundered digit and the second part is only the first number
-        //I can hardcode the valous of the array because I know that they will always be two
-
-        //todo remove the priniting
-        System.out.println( "part 1: "+splitVal[0]);
-        System.out.println( "part 2: "+splitVal[1]);
-
-        //todo convert these the first one to an int and ammend the secon one
-        int firstpartOfStrinAsInt = Integer.parseInt(splitVal[0]);//converts the first part of the string to an  int
-
+        //converts to an Integer and then uses that Integer to get the corresponding letter in the array
         finalString = allLetersinOrder[Integer.parseInt(splitVal[0])];
 
-        finalString = finalString + splitVal[1];//X means that there is no value
-        return finalString;//todo replace this with an actuall return and not just void
+        //just addes the last number to the string
+        finalString = finalString + splitVal[1];//n means that there is no value
+        return finalString;
 
     }
 }
